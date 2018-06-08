36793b20a12c8784c1646ce3ee953aba7d255f56
Rik <rikbasting97@gmail.com>
2018-06-08 10:35:49 +0200

    added sendVoice function

---
+++ API/actions/send_voice.js
@@ -0,0 +1,11 @@
+const request = require('request');
+
+module.exports = {
+
+    sendVoice(){
+        return new Promise((resolve) => {
+            let headers = {
+                "Content-Type": "application/json",
+            };
+        });
+}
