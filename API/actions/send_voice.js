475a6bb584c22dfb71c31264604bf4b5f82a1add
Rik <rikbasting97@gmail.com>
2018-06-08 10:44:05 +0200

    added options

--- API/actions/send_voice.js
+++ API/actions/send_voice.js
@@ -2,10 +2,28 @@ const request = require('request');
 
 module.exports = {
 
-    sendVoice(){
+    sendVoice(callee, caller, anonymous, message){
         return new Promise((resolve) => {
             let headers = {
                 "Content-Type": "application/json",
+            };
+            let options = {
+                url: 'https://gw.cmtelecom.com/v1.0/message',
+                method: 'POST',
+                headers: headers,
+                json: true,
+                body: {
+                    "callee": "0031642338343",
+                    "caller": "0031657618707",
+                    "anonymous": false,
+                    "prompt": "Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? Hoe gaat het? ",
+                    "prompt-type": "TTS",
+                    "voice": {
+                        "language": "nl-NL",
+                        "gender": "Male",
+                        "number": 1
+                    }
+                }
             };
         });
 }
